ff9a7eb0610fb41e5a962681f64bf4feee27895f
EveilHub <EveilHub@proton.me>
2026-03-10 08:44:15 +0100

    code envolved

--- my-app/app/ui/search-bar.tsx
+++ my-app/app/ui/search-bar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SubmitEvent, JSX } from "react";
+import { SubmitEvent, JSX, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const SearchBar = ({ placeholder }: { placeholder: string }): JSX.Element => {
@@ -8,9 +8,11 @@ const SearchBar = ({ placeholder }: { placeholder: string }): JSX.Element => {
     const pathname: string = usePathname();
     const { replace } = useRouter();
     const searchParams = useSearchParams();
+    const router = useRouter();
+    const [error, setError] = useState<string | undefined>(undefined);
 
     const handleSearch = (term: string): void => {
-        console.log(term);
+        // console.log(term);
         const params = new URLSearchParams(searchParams);
         if (term) {
             params.set('query', term);
@@ -20,24 +22,41 @@ const SearchBar = ({ placeholder }: { placeholder: string }): JSX.Element => {
         replace(`${pathname}?${params.toString()}`);
     };
 
-    const validateSearch = (e: SubmitEvent<HTMLFormElement>): void => {
+    const validateSearch = (e: SubmitEvent<HTMLFormElement>): void | JSX.Element => {
         e.preventDefault();
-        const value = e.target.value;
-        console.log(value);
+
+        const formData = new FormData(e.currentTarget);
+        const value = formData.get("query");
+
+        if (value === "search" || value === "contact" || value === "images") {
+            router.push(`/${value}`);
+        } else {
+            setError("! Erreur Page introuvable !");
+        }
+    };
+
+    if (error) {
+        return (<>
+            <p style={{color: "red"}}>{error}</p>
+            <button type="button" onClick={() => setError("")} style={{marginLeft: "20px"}}>Refresh</button>
+        </>);
     };
 
     return (
-        <form onSubmit={(e) => validateSearch(e)} className="w-2/3 flex flex-row border border-cyan-400">
+        <form onSubmit={(e) => validateSearch(e)} className="w-2/3 flex flex-row">
             <input
-                className="peer block w-[85%] rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                name="query"
+                className="peer block w-[90%] rounded-md py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 placeholder={placeholder}
                 onChange={(e) => {
                     handleSearch(e.target.value);
                 }}
                 defaultValue={searchParams.get('query')?.toString()}
             />
-            <button type="submit" className="w-[15%] bg-blue-500 m-left">Enter</button>
+            <button type="submit" className="w-[10%] bg-blue-500 mx-4 rounded-md hover:bg-blue-600 active:bg-blue-400">
+                Enter
+            </button>
         </form>
     )
-}
+};
 export default SearchBar;
