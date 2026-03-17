81448b27ca569ba10679ee56362768591a27c162
EveilHub <EveilHub@proton.me>
2026-03-17 15:41:53 +0100

    new route created

---
+++ my-app/app/translation/page.tsx
@@ -0,0 +1,25 @@
+import { JSX, Suspense } from "react";
+import BtnHome from "../ui/btn-home";
+import SearchBar from "../ui/search-bar";
+import Loading from "./loading";
+
+const TranslationPage = (): JSX.Element => {
+
+    return (
+        <div className="w-full h-full">
+
+            <div className="flex justify-between text-3xl font-bold p-10">
+                <h1>Search Page</h1>
+                <BtnHome />
+            </div>
+
+            <div className="flex items-center justify-center">
+                <Suspense fallback={<Loading />}>
+                    <SearchBar />
+                </Suspense>
+            </div>
+
+        </div>
+    )
+};
+export default TranslationPage;
