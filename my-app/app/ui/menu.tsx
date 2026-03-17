6b383b0a830e690c2337426a908c4a63aba7acbe
EveilHub <EveilHub@proton.me>
2026-03-17 15:44:24 +0100

    modified

--- my-app/app/ui/menu.tsx
+++ my-app/app/ui/menu.tsx
@@ -6,7 +6,8 @@ const Menu = (): JSX.Element => {
         <div className="w-full h-[8vh] flex flex-row items-center justify-around text-lg font-bold bg-slate-800/70">
             <Link href="/" className="text-cyan-400 hover:text-cyan-500 active:text-cyan-300">Home</Link>
             <Link href="/images" className="text-cyan-400 hover:text-cyan-500 active:text-cyan-300">Images</Link>
-            <Link href="/search" className="text-cyan-400 hover:text-cyan-500 active:text-cyan-300">Search</Link>
+            <Link href="/translation" className="text-cyan-400 hover:text-cyan-500 active:text-cyan-300">Translation</Link>
+            <Link href="/file" className="text-cyan-400 hover:text-cyan-500 active:text-cyan-300">Translate File</Link>
             <Link href="/contact" className="text-cyan-400 hover:text-cyan-500 active:text-cyan-300">Contact</Link>
         </div>
     )
